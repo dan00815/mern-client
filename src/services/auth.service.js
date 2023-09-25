f13b532bab42f0e3ab92268f3fc30c6995faf5dd
Dan Wu <Wu@gmail.com>
2023-09-25 19:27:47 +0800

    client commit

---
+++ src/services/auth.service.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+const API = "https://mern-server-25tt.onrender.com/api/user";
+
+class AuthService {
+  register(username, email, password, role) {
+    return axios.post(API + "/register", { username, email, password, role });
+  }
+
+  login(email, password) {
+    return axios.post(API + "/login", { email, password });
+  }
+
+  logout() {
+    localStorage.removeItem("mainkey");
+  }
+
+  getCurrentUser() {
+    return JSON.parse(localStorage.getItem("mainkey"));
+  }
+}
+
+export default new AuthService();
